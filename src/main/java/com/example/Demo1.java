478707af51535d14651f23d460ad5ae69156e976
stardujie <stardujie@163.com>
2020-03-07 22:51:55 +0800

    first commit

---
+++ src/main/java/com/example/Demo1.java
@@ -0,0 +1,21 @@
+package com.example;
+
+/**
+ * Created by jizhi.qian on 2018/11/14.
+ */
+public class Demo1 {
+    public int intAdd(int a, int b){
+        int r = a + b;
+        return r;
+    }
+
+    public int intSubtract(int a, int b){
+        int r = a - b;
+        return r;
+    }
+
+    public long intMultiply(int a, int b){
+        long r = a * b;
+        return r;
+    }
+}
